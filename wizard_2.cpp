48f32491b84347d42b5820003d9dfe90f24451e5
duergar85 <duergar85@gmail.com>
2011-12-08 11:02:41 +0100

    Added a file with the GPL 3.0 and added the GPL informations at the beginning of every file

--- wizard_2.cpp
+++ wizard_2.cpp
@@ -1,3 +1,21 @@
+/*
+ * QTSpyder - An interface to use Dalsa Spyder3 1k, 36 kHz GigE camera
+ * Copyright (C) 2011-2012 DRDSoftware
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "wizard_2.h"
 #include "ui_wizard_2.h"
 
