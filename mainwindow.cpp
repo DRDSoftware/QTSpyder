2e07513b801b4ba14d0b60baed422fc4e2fbab90
duergar85 <duergar85@gmail.com>
2011-12-10 12:04:47 +0100

    Updated the UI of the main window of the program

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,11 +19,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QStyle>
+#include <QDesktopWidget>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setGeometry(
+        QStyle::alignedRect(
+            Qt::LeftToRight,
+            Qt::AlignCenter,
+            this->size(),
+            qApp->desktop()->availableGeometry()
+        ));
     this->setWindowState(Qt::WindowMaximized);
     about = NULL;
     /// \todo This should only be done if the user requested
